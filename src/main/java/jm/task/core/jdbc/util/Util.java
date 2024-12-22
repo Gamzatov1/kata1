ffdeac42029dcc53ebece2db765f61bef31ada13
Гамзатов Ибрагим <ibr.gamzatov.090905@mail.ru>
2024-12-22 18:36:23 +0300

    Исправил ошибки

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,23 +5,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+
+    private Util() {
+    }
+
     private final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private final static String URL = "jdbc:mysql://localhost:3306/?user=root";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "katacon";
 
+    private static Connection connection = null;
+
     public static Connection getConnection() {
+
         {
-            try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)){
+            try {
                 Class.forName(DB_DRIVER);
-                return connection;
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             } catch (SQLException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
-        return null;
+        return connection;
     }
 
-
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
